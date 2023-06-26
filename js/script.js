e89b304a9c04f3e57186edd03e71577de6890187
azizElb444 <aziz_elbelaychy@um5.ac.ma>
2023-06-26 05:28:15 +0200

    files list

--- js/script.js
+++ js/script.js
@@ -70,3 +70,26 @@ new Typewriter('#typewriter', {
   loop:true,
   cursor:"|"
 });
+
+//===========================Projects Swiper=========================
+
+var swiper = new Swiper(".blog-slider", {
+      spaceBetween: 30,
+      effect: 'fade',
+      loop : true,
+      mousewheel : {
+            invert:false,
+      },
+     // navigation: {
+       // nextEl: ".swiper-button-next",
+       // prevEl: ".swiper-button-prev",
+     // },
+      pagination: {
+        el: ".blog-slider__pagination",
+        clickable: true,
+      },
+      //mousewheel: true,
+      keyboard: true,
+    });
+
+
