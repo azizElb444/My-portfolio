8cf2f3e654ae14446daee60daa537f9a4beb5398
azizElb444 <aziz_elbelaychy@um5.ac.ma>
2023-07-06 04:27:54 +0200

    files list

--- js/script.js
+++ js/script.js
@@ -89,7 +89,45 @@ var swiper = new Swiper(".blog-slider", {
         clickable: true,
       },
       //mousewheel: true,
-      keyboard: true,
-    });
+     });
 
 
+
+//=============================GOING UP======================================
+
+function scrollUp() {
+    const scrollup =document.getElementById('scroll-up');
+    //when scrolling has exceeded 560px in height , the arrow-up icon shows up
+    if (this.scrollY >=560) {
+        scrollup.classList.add('show-scroll');
+    }
+    else {
+        scrollup.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll',scrollUp);
+
+//=========================SCROLL sections highlight==============================
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50 ;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }
+        else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+
+    })
+
+}
+window.addEventListener('scroll',scrollActive);
